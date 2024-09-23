6dc349f0fb265b40b291be009cc03ca79f203f38
NezdanchikG <NezdanchikTHANOS@gmail.com>
2024-09-23 21:04:38 +0300

    -

--- src/main/java/com/Nezdanchik/spbpu/config/MongoConfig.java
+++ src/main/java/com/Nezdanchik/spbpu/config/MongoConfig.java
@@ -1,7 +1,6 @@
 package com.Nezdanchik.spbpu.config;
 
 import com.mongodb.MongoClient;
-import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +8,9 @@ import org.springframework.context.annotation.Configuration;
 public class MongoConfig {
 
     @Bean
-    @SneakyThrows
     public MongoClient mongoClient() {
-        return new MongoClient();
+        return mongoClient();
     }
+
 
 }
